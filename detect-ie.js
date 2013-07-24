cff3dd5a6499262bf3b5c39e5196eddd1a13efa4
Ian Gall <ianpgall@gmail.com>
2013-07-24 12:08:29 -0400

    Update detect-ie.js

--- detect-ie.js
+++ detect-ie.js
@@ -1,28 +1,32 @@
 var IE = (function () {
     "use strict";
-    
-    var isTheBrowser = false,
-        actualVersion, currentVersion,
-        jscriptMap, jscriptVersion;
-    
-    jscriptMap = {
-        "5.5": "5.5",
-        "5.6": "6",
-        "5.7": "7",
-        "5.8": "8",
-        "9": "9",
-        "10": "10"
-    };
-    jscriptVersion = Function("/*@cc_on return @_jscript_version; @*/")();
-    if (typeof jscriptVersion !== "undefined") {
-        isTheBrowser = true;
-        actualVersion = jscriptMap[jscriptVersion];
-        currentVersion = "" + document.documentMode;
-    }
-    
-    return {
-        isTheBrowser: isTheBrowser,
-        actualVersion: actualVersion,
-        currentVersion: currentVersion
-    };
-})();
+
+	var ret, isTheBrowser,
+		actualVersion, currentVersion,
+		jscriptMap, jscriptVersion;
+
+	isTheBrowser = false;
+	jscriptMap = {
+		"5.5": "5.5",
+		"5.6": "6",
+		"5.7": "7",
+		"5.8": "8",
+		"9": "9",
+		"10": "10"
+	};
+	jscriptVersion = new Function("/*@cc_on return @_jscript_version; @*/")();
+
+	if (typeof jscriptVersion !== "undefined") {
+		isTheBrowser = true;
+		actualVersion = jscriptMap[jscriptVersion];
+		currentVersion = "" + document.documentMode;
+	}
+
+	ret = {
+		isTheBrowser: isTheBrowser,
+		actualVersion: actualVersion,
+		currentVersion: currentVersion
+	};
+
+	return ret;
+}());
